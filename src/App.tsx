40a8297df71ccd66a4a1680ba0153302877cc01b
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-03-15 14:03:53 -0300

    header portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export function App() {
-  return <h1>Leonardo Lima</h1>;
+  return (
+    <div className="bg-dark w-full py-2 px-1.5">
+      <div className="container m-auto">
+        <div className="flex w-fit m-auto gap-1 mb-2.5 md:hidden">
+          <img
+            className="hover:cursor-pointer"
+            src="/assets/svg/linkedin.svg"
+            alt="icone linkedin"
+          />
+          <img
+            className="hover:cursor-pointer"
+            src="/assets/svg/github.svg"
+            alt="icone githun"
+          />
+        </div>
+
+        <div className="hidden md:flex container m-auto bg-purple-5 h-4 rounded-lg px-1.5 items-center justify-between">
+          <img
+            src="/assets/svg/logo.svg"
+            alt="logo"
+            className="w-2.5 max-h-2.5 block hf"
+          />
+          <div className="flex w-fit gap-x-1.5">
+            <button className="btn-menu active">Home</button>
+            <button className="btn-menu">Sobre mim</button>
+            <button className="btn-menu">Portfólio</button>
+            <button className="btn-menu">Skills</button>
+            <button className="btn-menu">Carreira</button>
+            <button className="btn-menu">Recomendações</button>
+            <button className="btn-menu">Contato</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
       "purple-100": "#7B4AE2",
       "purple-10": "rgba(123,74, 226, 0.1)",
+      "purple-5": "rgba(123,74, 226, 0.05)",
+      "purple-50": "rgba(123,74, 226, 0.5)",
       "purple-20": "rgba(123,74, 226, 0.2)",
       "withe-100": "#FFFFFF",
       "withe-50": "rgba(255,255, 255, 0.5)",
       dark: "#090E16",
       "green-100": "#4AE290",
       "green-30": "rgba(74,256, 144, 0.3)",
+    },
+    spacing: {
+      1: "1rem", //16px
+      1.5: "1.5rem", //24px
+      2: "2rem", //32px
+      2.5: "2.5rem", //40px
+      3: "3rem", //48px
+      4: "4rem", //64px
+      5: "5rem", //80px
+      6: "6rem", //96px
     },
 
     extend: {},
