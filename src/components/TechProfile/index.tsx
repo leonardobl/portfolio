1188c266884ce9e74302e1e9a59af8d187751a18
Leonardo Lima <leonardo.b.lima1@gmail.com>
2025-03-03 07:48:38 -0300

    adjust padding techSumary

--- src/components/TechProfile/index.tsx
+++ src/components/TechProfile/index.tsx
@@ -3,7 +3,7 @@ import { Title } from "../Typography/Title";
 
 export const TechProfile = () => {
   return (
-    <div className="bg-custom-gray-100 w-full max-w-[370px] flex flex-col px-10 py-14 gap-4 rounded-[120px]">
+    <div className="bg-custom-gray-100 w-full max-w-[400px] flex flex-col px-10 py-14 gap-4 rounded-[120px]">
       <div className="flex gap-3 items-center">
         <Title as="h2" className="text-custom-brand-100 text-5xl">
           4
